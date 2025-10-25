814e54bed8e32a815fc564b30147bbc90e7f4588
Oluwasegun Lawrence <lawrencesegun025@gmail.com>
2025-10-25 09:45:03 +0100

    added dockerfile

--- web/src/routes/index.tsx
+++ web/src/routes/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router";
-import { Header } from "../components/header";
-import { Hero } from "../components/hero";
-import { Features } from "../components/features";
-import { CTA } from "../components/cta";
-import { Footer } from "../components/footer";
+import { Header } from "@/components/header";
+import { Hero } from "@/components/hero";
+import { Features } from "@/components/features";
+import { CTA } from "@/components/cta";
+import { Footer } from "@/components/footer";
 
 export const Route = createFileRoute("/")({ component: Index });
 
